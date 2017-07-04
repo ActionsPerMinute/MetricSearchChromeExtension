3b31c7e759f725fec4511ee5f5adccfa932ed4a1
ActionsPerMinute <toby.davies@AppDynamics.com>
2017-07-04 14:46:00 +0100

    fix running on all pages
    background does not have matches, like content_scripts

--- background.js
+++ background.js
@@ -1,5 +1,15 @@
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {//add listener to the url
-   chrome.tabs.executeScript( null, { 
-        file: "content.js"
+    chrome.tabs.query({
+      active: true,
+      currentWindow: true
+    }, function(tabs) {
+        var tab = tabs[0];
+        var url = tab.url;
+        if(url.indexOf("METRIC_BROWSER") > -1){
+            chrome.tabs.executeScript( null, {
+                file: "content.js"
+            });
+        }
     });
+    
 })
